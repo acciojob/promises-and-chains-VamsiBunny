2ff3f5ed235fcd5a37fef3621e85fcb03c95915b
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-12-14 20:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+let age=document.getElementById("age");
+let name=document.getElementById("name");
+let btn=document.getElementById("btn");
+
+btn.addEventListener("click",Submit());
+
+function Submit() {
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		if(age.value>18){
+			resolve(`Welcome ${name}. You can vote.`);
+		}else{
+			reject(`Oh sorry ${name}. You aren't old enough.`);
+		}
+	},4000)
+})
+}
+	
+
